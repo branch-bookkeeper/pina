424b18e0b259266cc18bbff98721de5a81e44054
Gabriele Genta <gabriele.genta@gmail.com>
2017-09-07 19:41:26 +0200

    Redirect to stored path on login success

--- src/pages/OAuthSuccess.jsx
+++ src/pages/OAuthSuccess.jsx
@@ -1,6 +1,9 @@
 import qs from 'qs';
+import defaultTo from 'ramda/src/defaultTo';
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+
+import { KEY_LOGIN_REDIRECT_URL } from '../constants/localStorageKeys';
 
 const OAuthSuccess = ({ location: { search: queryString } }) => {
     if (queryString.length && queryString[0] === '?') {
@@ -9,9 +12,10 @@ const OAuthSuccess = ({ location: { search: queryString } }) => {
             localStorage.setItem('gh-token', query.access_token);
         }
     }
+    const redirectPath = defaultTo('/', localStorage.getItem(KEY_LOGIN_REDIRECT_URL));
 
     return (
-        <Redirect to="/"/>
+        <Redirect to={redirectPath}/>
     );
 };
 
