5e304c3e63bd490b8c5e8d9a2c3d3f8a345df100
Gabriele Genta <gabriele.genta@gmail.com>
2017-09-09 17:08:35 +0200

    Replace static API url with configurable value

--- src/helpers/generateBranchQueueUrl.js
+++ src/helpers/generateBranchQueueUrl.js
@@ -1,8 +1,9 @@
+import { API_BASE_URL } from '../constants/config';
 import join from 'ramda/src/join';
 
 export default (owner, repository, branch) =>
     join('/', [
-        'https://cors-anywhere.herokuapp.com/https://branch-bookkeeper.herokuapp.com/api',
+        API_BASE_URL,
         owner,
         repository,
         branch
