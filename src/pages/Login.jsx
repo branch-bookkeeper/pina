c20536ce12b928343c459c375beb45c376a8d084
Gabriele Genta <gabriele.genta@gmail.com>
2017-09-09 18:30:52 +0200

    Move github client id to configuration

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { KEY_LOGIN_REDIRECT_URL } from '../constants/localStorageKeys';
+import { GITHUB_CLIENT_ID } from '../constants/config';
 
 const Login = ({ location: { pathname, search, hash } }) => {
     localStorage.setItem(KEY_LOGIN_REDIRECT_URL, [pathname, search, hash].join(''));
 
     return (
         <div>
-            <a href="https://github.com/login/oauth/authorize?client_id=Iv1.ae8d32938d68d499">Login with GitHub</a>
+            <a href={`https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}`}>
+                Login with GitHub
+            </a>
         </div>
     );
 };
