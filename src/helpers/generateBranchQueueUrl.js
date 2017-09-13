efd340bfda2224b045898b7ec8e7b05cc0331d33
Gabriele Genta <gabriele.genta@gmail.com>
2017-09-13 19:26:51 +0200

    Align api url to backend

--- src/helpers/generateBranchQueueUrl.js
+++ src/helpers/generateBranchQueueUrl.js
@@ -4,6 +4,7 @@ import join from 'ramda/src/join';
 export default (owner, repository, branch) =>
     join('/', [
         API_BASE_URL,
+        'queue',
         owner,
         repository,
         branch
