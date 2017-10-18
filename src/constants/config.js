6a9e6ba99d80fe26a8c17a4e7efc87219a821c82
Gabriele Genta <gabriele.genta@gmail.com>
2017-10-18 22:59:00 +0200

    Add OneSignal application ids

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,2 +1,3 @@
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
 export const GITHUB_CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID || 'Iv1.435916aff1ff1c91';
+export const ONESIGNAL_APP_ID = process.env.REACT_APP_ONESIGNAL_APP_ID || '5c757281-e7d6-4c44-bb13-b12d01fac593';
