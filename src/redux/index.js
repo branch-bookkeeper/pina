662ba445b8e80798d3cc213f0f8a3521246abf2f
Gabriele Genta <gabriele.genta@gmail.com>
2017-10-18 22:59:58 +0200

    Add push redux module

--- src/redux/index.js
+++ src/redux/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import user from './user';
 import requests, { stateShape as requestsShape } from './requests';
 import entities, { stateShape as entitiesShape } from './entities';
+import push, { stateShape as pushShape } from './push';
 
 const rootReducer = combineReducers({
     user,
     requests,
     entities,
+    push,
 });
 
 export default rootReducer;
@@ -15,4 +17,5 @@ export default rootReducer;
 export {
     requestsShape,
     entitiesShape,
+    pushShape,
 };
