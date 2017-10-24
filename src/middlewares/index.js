8aa44fe710acf72cab2799a97a3cfdecaa6a0f9c
Gabriele Genta <gabriele.genta@gmail.com>
2017-10-24 09:15:47 +0200

    Add redux-logger in dev env

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,14 +1,20 @@
+import apply from 'ramda/src/apply';
 import { applyMiddleware } from 'redux';
 import { createEpicMiddleware, combineEpics } from 'redux-observable';
 
 import { epics as requestsEpics } from '../redux/requests';
 import { epics as pushEpics } from '../redux/push';
 
-export default applyMiddleware(
+const devMiddlewares = process.env.NODE_ENV === 'development'
+    ? [require('redux-logger').default]
+    : [];
+
+export default apply(applyMiddleware, [
+    ...devMiddlewares,
     createEpicMiddleware(
         combineEpics(
             requestsEpics,
             pushEpics,
         ),
     ),
-);
+]);
