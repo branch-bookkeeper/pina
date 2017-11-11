58784068664bb77b73b5e40825b4888ce9c249e4
Gabriele Genta <gabriele.genta@gmail.com>
2017-11-11 09:36:05 +0100

    Add signature color palette

---
+++ src/constants/colors.js
@@ -0,0 +1,15 @@
+import { orange } from 'material-ui/colors';
+
+const bbOrange = { ...orange };
+bbOrange[50] = '#fae7e1';
+bbOrange[100] = '#facabc';
+bbOrange[200] = '#faad96';
+bbOrange[300] = '#fa8764';
+bbOrange[400] = '#fa744b';
+bbOrange[500] = '#f96232';
+bbOrange[600] = '#f35e30';
+bbOrange[700] = '#e6592e';
+bbOrange[800] = '#d9542b';
+bbOrange[900] = '#bf4a26';
+
+export { bbOrange };
