3f639da3374f63d4ee58d20d89ff7d73122d339d
Gabriele Genta <gabriele.genta@gmail.com>
2017-11-13 22:28:39 +0100

    Read authentication data also from URL hash

--- src/pages/OAuthSuccess.jsx
+++ src/pages/OAuthSuccess.jsx
@@ -1,13 +1,33 @@
 import qs from 'qs';
 import defaultTo from 'ramda/src/defaultTo';
+import compose from 'ramda/src/compose';
+import filter from 'ramda/src/filter';
+import head from 'ramda/src/head';
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 import { GITHUB_ACCESS_TOKEN, KEY_LOGIN_REDIRECT_URL } from '../constants/localStorageKeys';
 
-const OAuthSuccess = ({ location: { search: queryString } }) => {
-    if (queryString.length && queryString[0] === '?') {
-        const query = qs.parse(queryString.substr(1));
+const stripStartingChar = (char, str) => {
+    if (str && str.length && str[0] === char) {
+        return str.substr(1);
+    } else {
+        return str;
+    }
+}
+
+const OAuthSuccess = ({ location }) => {
+    const { search, hash } = location;
+    const queryString = compose(
+        head,
+        filter(input => input && input.length),
+    )([
+        stripStartingChar('?', search),
+        stripStartingChar('#', hash),
+    ]);
+
+    if (queryString.length) {
+        const query = qs.parse(queryString);
         if (query.access_token) {
             localStorage.setItem(GITHUB_ACCESS_TOKEN, query.access_token);
         }
