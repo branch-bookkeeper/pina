0f6bba47990ebb57efa99e1f947f8a0ff93dfc9d
Gabriele Genta <gabriele.genta@gmail.com>
2017-11-13 21:15:08 +0100

    Add ui actions for showing/hiding snackbars

--- src/redux/ui.js
+++ src/redux/ui.js
@@ -7,22 +7,39 @@ import PropTypes from 'prop-types';
 // Constants
 export const OPEN_SETTINGS_DIALOG = 'OPEN_SETTINGS_DIALOG';
 export const CLOSE_SETTINGS_DIALOG = 'CLOSE_SETTINGS_DIALOG';
+export const SHOW_SNACKBAR = 'SHOW_SNACKBAR';
+export const HIDE_SNACKBAR = 'HIDE_SNACKBAR';
 
 export const stateShape = PropTypes.shape({
     settingsDialog: PropTypes.shape({
         show: PropTypes.bool.isRequired,
     }).isRequired,
+    snackbar: PropTypes.shape({
+        show: PropTypes.bool.isRequired,
+        message: PropTypes.string,
+        actionDescription: PropTypes.string,
+        action: PropTypes.shape({
+            type: PropTypes.string.isRequired,
+            payload: PropTypes.object,
+        }),
+    }),
 });
 
 const initialState = {
     settingsDialog: {
         show: false,
     },
+    snackbar: {
+        show: false,
+        message: null,
+        actionDescription: null,
+        action: null,
+    },
 };
 
 // Reducer
 export default (state = initialState, action) => {
-    const { type } = action;
+    const { type, payload } = action;
 
     switch (type) {
         case OPEN_SETTINGS_DIALOG:
@@ -35,6 +52,26 @@ export default (state = initialState, action) => {
         case CLOSE_SETTINGS_DIALOG:
             state = evolve({
                 settingsDialog: {
+                    show: F,
+                },
+            }, state);
+            break;
+        case SHOW_SNACKBAR: {
+            const { message, actionDescription, action } = payload;
+            state = {
+                ...state,
+                snackbar: {
+                    show: true,
+                    message,
+                    actionDescription,
+                    action,
+                },
+            };
+            break;
+        }
+        case HIDE_SNACKBAR:
+            state = evolve({
+                snackbar: {
                     show: F,
                 },
             }, state);
@@ -53,4 +90,17 @@ export const openSettingsDialog = always({
 
 export const closeSettingsDialog = always({
     type: CLOSE_SETTINGS_DIALOG,
+});
+
+export const showSnackbar = (message, actionDescription = null, action = null) => ({
+    type: SHOW_SNACKBAR,
+    payload: {
+        message,
+        actionDescription,
+        action,
+    },
+});
+
+export const hideSnackbar = always({
+    type: HIDE_SNACKBAR,
 });
