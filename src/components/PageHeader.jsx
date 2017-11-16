7838acf1943fa2f7a606ecb3d4ff9381099ed10f
Gabriele Genta <gabriele.genta@gmail.com>
2017-11-16 09:20:11 +0100

    Add generic components for page header and content

---
+++ src/components/PageHeader.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Grid from 'material-ui/Grid';
+import { withStyles } from 'material-ui/styles';
+
+const styles = theme => ({
+    root: {
+        padding: '84px 20px 20px 20px',
+        marginBottom: 24,
+        backgroundColor: 'white',
+    },
+});
+
+const PageHeader = ({ classes, children }) => (
+    <div className={classes.root}>
+        <Grid container justify="center">
+            <Grid item xs={12} sm={9} md={8} lg={6} xl={4}>
+                {children}
+            </Grid>
+        </Grid>
+    </div>
+);
+
+export default withStyles(styles)(PageHeader);
