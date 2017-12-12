815fc51cb53228eed2b87bf790b2e3e80b51eb18
Gabriele Genta <gabriele.genta@gmail.com>
2017-12-12 19:25:25 +0100

    Use welcome screen in home when there are no repos

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -3,16 +3,20 @@ import values from 'ramda/src/values';
 import prop from 'ramda/src/prop';
 import sortBy from 'ramda/src/sortBy';
 import { connect } from 'react-redux';
+import { branch, renderComponent } from 'recompose';
 
 import { isMade } from '../helpers/request';
 import { loadRepositories } from '../redux/requests';
 
 import withPreloading from '../hocs/withPreloading';
+import Welcome from '../components/Welcome';
 import Home from '../pages/Home';
 
 const sortByFullName = sortBy(prop('full_name'));
 const isLoadingNeeded = ({ repositoriesRequest }) => !isMade(repositoriesRequest);
 const load = ({ loadRepositories, accessToken }) => loadRepositories(accessToken);
+
+const hasNoRepositories = ({ repositories }) => repositories.length === 0;
 
 const mapStateToProps = ({
     requests: { repositories: repositoriesRequest },
@@ -29,4 +33,5 @@ const mapDispatchToProps = {
 export default compose(
     connect(mapStateToProps, mapDispatchToProps),
     withPreloading(isLoadingNeeded, load),
+    branch(hasNoRepositories, renderComponent(Welcome)),
 )(Home);
