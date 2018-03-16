f0f8ff38e3682ef2143e4cd58b3dfc6a7597a98c
Gabriele Genta <gabriele.genta@gmail.com>
2018-03-16 19:39:53 +0100

    Add pusher app key to configuration

--- src/constants/config.js
+++ src/constants/config.js
@@ -2,3 +2,4 @@ export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localh
 export const GITHUB_CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID || 'Iv1.435916aff1ff1c91';
 export const ONESIGNAL_APP_ID = process.env.REACT_APP_ONESIGNAL_APP_ID || '5c757281-e7d6-4c44-bb13-b12d01fac593';
 export const GOOGLE_ANALYTICS_ID = 'UA-106748550-2';
+export const PUSHER_APP_KEY = 'f09b62ce1811767365f8';
