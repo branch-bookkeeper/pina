22f3f286c9f8079e098cbee605f9d9eb81d9223c
Gabriele Genta <gabriele.genta@gmail.com>
2019-02-28 22:38:14 +0100

    Add mouse event support to user avatar

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -6,18 +6,38 @@ const propTypes = {
     username: PropTypes.string.isRequired,
     size: PropTypes.number,
     className: PropTypes.string,
+    onMouseOver: PropTypes.func,
+    onMouseLeave: PropTypes.func,
+    onTouchStart: PropTypes.func,
+    onTouchEnd: PropTypes.func,
 };
 
 const defaultProps = {
     size: 40,
+    onMouseOver: null,
+    onMouseLeave: null,
+    onTouchStart: null,
+    onTouchEnd: null,
 };
 
-const UserAvatar = ({ username, className, size }) => (
+const UserAvatar = ({
+    username,
+    className,
+    size,
+    onMouseOver,
+    onMouseLeave,
+    onTouchStart,
+    onTouchEnd,
+}) => (
     <Avatar
         src={`https://github.com/${username}.png?size=${size*4}`}
         alt={username}
         className={className}
         style={{ width: size, height: size }}
+        onMouseOver={onMouseOver}
+        onMouseLeave={onMouseLeave}
+        onTouchStart={onTouchStart}
+        onTouchEnd={onTouchEnd}
     />
 );
 
